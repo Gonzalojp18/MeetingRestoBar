313a034d8daedc753e6bf70cf81190a67ec11b52
Gonzalo Palomo <pgonzalojose@gmail.com>
2024-10-25 16:03:37 -0300

    solving issues on categorysection

--- src/components/CategorySection.jsx
+++ src/components/CategorySection.jsx
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-=======
 
->>>>>>> siriusblack
 
 const CategorySection = ({ category, items }) => {
 
@@ -10,17 +7,10 @@ const CategorySection = ({ category, items }) => {
                 <h2 className="text-xl font-semibold">{category}</h2>
                 <div className="mt-4 grid gap-4 sm:grid-cols-2 lg:grid-cols-3">
                     {items.map((item) => (
-<<<<<<< HEAD
-                        <div key={item.id} className="bg-white p-4 rounded shadow box-lunch">
+                        <div key={item.id} className="bg-white p-4 rounded-lg shadow-md box-lunch">
                             <h3 className="text-2xl font-semibold mb-2 entrada">{item.name}</h3>
-                            <p className="mb-2">{item.description}</p>
-                            <p className="text-pink-890 text-base font-extrabold">${item.price.toFixed(3)}</p>
-=======
-                        <div key={item.id} className="p-2 rounded-lg shadow box-lunch">
-                            <h3 className="text-1xl font-semibold mb-2 entrada">{item.name}</h3>
-                            <p className="mb-2">{item.description}</p>
-                            <p className="text-pink-900 text-base font-extrabold">${item.price.toFixed(3)}</p>
->>>>>>> siriusblack
+                            <p className="text-gray-600 mb-2">{item.description}</p>
+                            <p className="text-amber-800 text-base font-extrabold">${item.price.toFixed(3)}</p>
                         </div>
                     ))}
                 </div>
