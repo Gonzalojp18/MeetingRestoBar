2ea5b1727c064ae663d73376368db65117bb2eae
Gonzalo Palomo <pgonzalojose@gmail.com>
2024-10-25 17:48:23 -0300

    made responsive

--- src/components/CategoryCoffe.jsx
+++ src/components/CategoryCoffe.jsx
@@ -7,7 +7,7 @@ const CategoryCoffe = ({ category, items }) => {
                 <h2 className="text-3xl font-semibold py-8">{category}</h2>
             <div className="mt-0 grid gap-2 sm:grid-cols-4 lg:grid-cols-6">
                 {items.map((item) => (
-                    <div key={item.id} className="p-4 flex box-coffe">
+                    <div key={item.id} className="p-4 flex box-coffe box-items">
                         <h3 className="text-1xl font-semibold mb-2 entrada">{item.name}</h3>
                         <p className="text-pink-50 mb-2">{item.description}</p>
                         <p className="text-pink-50 text-base font-extrabold">${item.price.toFixed(3)}</p>
--- src/components/Ensalada.jsx
+++ src/components/Ensalada.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import CategoryHealtly from './CategoryHealtly';
 import menuData from '../data/data';
 import deluxe from '../source/ensaladas/deluxe.webp';
-// import casa from '../source/ensaladas/casa.webp';
 
 const Ensaladas = () => {
     const saladData = {
@@ -10,13 +9,11 @@ const Ensaladas = () => {
     };
 
 return (
-    <section id="ensaladas">
-            <section className='brusquetas-section'>
-                <div className='title-brusquetas flex'>
-                    <h2 className='text-5xl w-40'>Ensaladas de la Casa</h2>
-                    <img className='w-50 px-5' src={deluxe} alt="" />
+    <section>
+                <div className='title-salad flex'>
+                    <h2 className='text-5xl'>Ensaladas de la Casa</h2>
+                    <img className='px-2 px-5' src={deluxe} alt="" />
                 </div>
-            </section>
     {Object.entries(saladData).map(([category, items]) => (
         <CategoryHealtly key={category} category={category} items={items} />
     ))}
