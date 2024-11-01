166e6445f16f900db51a30d36a5ddac2bb3fab5d
Gonzalo Palomo <pgonzalojose@gmail.com>
2024-11-01 16:01:44 -0300

    solving conflict

--- src/components/CategorySection.jsx
+++ src/components/CategorySection.jsx
@@ -8,15 +8,9 @@ const CategorySection = ({ category, items }) => {
                 <div className="mt-4 grid gap-4 sm:grid-cols-2 lg:grid-cols-3">
                     {items.map((item) => (
                         <div key={item.id} className="p-4 rounded box-lunch">
-<<<<<<< HEAD
                             <h3 className="text-xl font-bold mb-2 entrada">{item.name}</h3>
                             <p className="text-1xl font-semibold mb-2">{item.description}</p>
                             <p className="text-amber-600 text-base font-bold">${item.price.toFixed(3)}</p>
-=======
-                            <h3 className="text-l font-semibold mb-2 entrada">{item.name}</h3>
-                            <p className="mb-2">{item.description}</p>
-                            <p className="font-semibold text-l">${item.price.toFixed(3)}</p>
->>>>>>> 6d3630d76748cfd9716cebc9d384f80e8e1686d6
                         </div>
                     ))}
                 </div>
